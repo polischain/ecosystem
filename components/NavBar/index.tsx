711ae00511d2bc59d904e4da8169c3f924c591ea
eabz <eabz@polispay.org>
2021-09-03 07:30:16 -0500

    Base app

---
+++ components/NavBar/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+export default function Navbar(): JSX.Element {
+  const [navbarOpen, setNavbarOpen] = React.useState(false)
+  return (
+    <>
+      <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between lg:w-4/6">
+          <div className="flex items-center flex-1 justify-between lg:justify-start">
+            <a aria-label="Home" href="/">
+              <img
+                className="h-8 w-auto sm:h-10"
+                src="/img/logo.png"
+                alt="Logo"
+              />
+            </a>
+            <a
+              className="text-xl font-bold leading-6 text-white sm:truncate ml-2"
+              href="/"
+            >
+              Polis
+            </a>
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              {navbarOpen ? (
+                <i className="text-white fas fa-times" />
+              ) : (
+                <i className="text-white fas fa-bars" />
+              )}
+            </button>
+          </div>
+
+          <div
+            className={
+              'mt-3 lg:flex flex-grow items-center lg:bg-opacity-0 lg:shadow-none' +
+              (navbarOpen ? 'block rounded shadow-lg bg-dark' : ' hidden')
+            }
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="flex items-center text-white text-md my-3">
+                <a href="https://doc.polis.tech" rel="noopener noreferrer">
+                  <span className="inline-block ml-4">Documentation</span>
+                </a>
+              </li>
+              <li className="flex items-center text-white text-md my-3">
+                <a href="#about">
+                  <span className="inline-block ml-4">About</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
---
+++ pages/_app.tsx
@@ -0,0 +1,85 @@
+import '../styles/index.css'
+import '../styles/styles.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+import Head from 'next/head'
+import React from 'react'
+
+function MyApp({ Component, pageProps }): JSX.Element {
+  return (
+    <React.Fragment>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <title key="title">Polis Chain</title>
+
+        <meta
+          key="description"
+          name="description"
+          content="Join the payments revolution with our EVM powered network and community"
+        />
+
+        <meta name="application-name" content="Polis Chain" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="apple-mobile-web-app-title" content="Polis Chain" />
+
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#3E4A94" />
+
+        <meta key="twitter:card" name="twitter:card" content="app" />
+        <meta key="twitter:title" name="twitter:title" content="Polis Chain" />
+        <meta
+          key="twitter:url"
+          name="twitter:url"
+          content="https://polis.tech"
+        />
+        <meta
+          key="twitter:description"
+          name="twitter:description"
+          content="Join the payments revolution with our EVM powered network and community"
+        />
+        <meta
+          key="twitter:image"
+          name="twitter:image"
+          content="https://polis.tech/manifest-icon-192.png"
+        />
+        <meta
+          key="twitter:creator"
+          name="twitter:creator"
+          content="@PolisChain"
+        />
+        <meta key="og:type" property="og:type" content="website" />
+        <meta
+          key="og:site_name"
+          property="og:site_name"
+          content="Polis Chain"
+        />
+        <meta key="og:url" property="og:url" content="https://polis.tech" />
+        <meta
+          key="og:image"
+          property="og:image"
+          content="https://polis.tech/apple-icon-180.png"
+        />
+        <meta
+          key="og:description"
+          property="og:description"
+          content="Join the payments revolution with our EVM powered network and community"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
+}
+
+export default MyApp
